4ed243f57cf7adeffbee424a2f7a57223db99709
shabani005 <hasan.shabani2005@gmail.com>
2025-07-21 02:50:36 +0300

    the base version is basically finished

--- main.c
+++ main.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
-
-int main(int argc, char *argv[]){
-  FILE *filepointer;
+int main(int argc, char **argv){
   int chars; 
-  
+ 
+  typedef struct{
+    FILE **fpArr;
+    int chars;
+  }CC_FILES;
 
-  // filepointer = fopen("1g.txt", "r");
-  filepointer = fopen(argv[1], "r");
+  CC_FILES instance;
 
-  // implement multiple argv's 
-  // implement multiple threads that divides the process
 
-  if (filepointer == NULL){
+ instance.fpArr = malloc(sizeof(FILE*) * argc);
+
+if (instance.fpArr == NULL){
     printf("couldnt open file");
     return 1;
   }
-  
-  while ((chars = fgetc(filepointer)) != EOF){
-    char msg2 = chars;
-    printf("%c", msg2);
-  }
 
-  fclose(filepointer);
+
+  for (int i=0; i < argc-1; i++){
+    instance.fpArr[i] = fopen(argv[i+1], "r");
+    while ((chars = fgetc(instance.fpArr[i])) != EOF){
+      char msg2 = chars;
+      printf("%c", msg2);
+    }
+    fclose(instance.fpArr[i]);
+  }
+  // filepointer = fopen("1g.txt", "r");
+  //filepointer = fopen(argv[1], "r");
+
+  // implement multiple argv's 
+  // implement multiple threads that divides the process
   
 
   return 0;
