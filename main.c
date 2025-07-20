9165fcb2495389095304b739ba2f727fcb68c928
shabani005 <hasan.shabani2005@gmail.com>
2025-07-21 02:55:06 +0300

    cleaned code

--- main.c
+++ main.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int main(int argc, char **argv){
-  int chars; 
- 
-  typedef struct{
+ typedef struct{
     FILE **fpArr;
     int chars;
   }CC_FILES;
 
+// implement multithreading that divides the process
+int main(int argc, char **argv){
+ 
   CC_FILES instance;
+  instance.fpArr = malloc(sizeof(FILE*) * argc);
 
-
- instance.fpArr = malloc(sizeof(FILE*) * argc);
-
-if (instance.fpArr == NULL){
+  if (instance.fpArr == NULL){
     printf("couldnt open file");
     return 1;
   }
 
-
+  int chars;
   for (int i=0; i < argc-1; i++){
     instance.fpArr[i] = fopen(argv[i+1], "r");
     while ((chars = fgetc(instance.fpArr[i])) != EOF){
@@ -28,13 +25,7 @@ if (instance.fpArr == NULL){
       printf("%c", msg2);
     }
     fclose(instance.fpArr[i]);
-  }
-  // filepointer = fopen("1g.txt", "r");
-  //filepointer = fopen(argv[1], "r");
-
-  // implement multiple argv's 
-  // implement multiple threads that divides the process
-  
+  }  
 
   return 0;
 }
